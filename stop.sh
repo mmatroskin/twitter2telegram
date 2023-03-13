c16b8facf826ff278a8e864aab96ef60ab402b55
MMatroskin <kotmatrosskin82@gmail.com>
2023-03-14 01:36:08 +0300

    dev: fix deploy script - kill all process

--- stop.sh
+++ stop.sh
@@ -2,7 +2,7 @@ echo -e $PWD
 
 echo -e '\e[1m\e[34mStopping processes...\e[0m\n'
 
-pids=$(ps axu |grep python | grep ~/apps/twitter2telegram/main.py | awk {'print $2'})
+pids=$(ps axu |grep python | grep ~/apps/twitter2telegram | awk {'print $2'})
 
 for pid in $pids
 do
