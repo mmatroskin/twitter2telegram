eb50d3154adc641749551ee6b8c5f0d008698f3c
MMatroskin <kotmatrosskin82@gmail.com>
2023-03-14 09:59:28 +0300

    dev: debug deploy script

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,8 @@ echo -e $PWD
 
 echo -e $USER
 
+echo -e $BOT_ADMIN
+
 echo -e '\e[1m\e[34mStopping processes...\e[0m\n'
 
 pids=$(ps axu |grep python | grep ~/apps/twitter2telegram | awk {'print $2'})
